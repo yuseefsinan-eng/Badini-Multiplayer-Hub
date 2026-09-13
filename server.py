4ae0539e9d66005d1e9cca741c68e1a8cd79763a
yuseefsinan-eng <yuseefsinan@gmail.com>
2026-09-14 01:46:38 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+badini_lang = {
+    "server_name": "سێرڤەرا کوردییا بادینی - SAMP",
+    "status": "سێرڤەر کاردکەت و ب خێر هاتن!"
+}
+
+@app.route('/')
+def home():
+    return jsonify(badini_lang)
+
+@app.route('/settings/language', methods=['POST'])
+def change_language():
+    data = request.json
+    selected_lang = data.get('lang', 'badini')
+    return jsonify({"message": f"زمان هاتە گوهۆڕین بۆ: {selected_lang}"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
