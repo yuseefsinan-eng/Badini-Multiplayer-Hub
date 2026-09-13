e6409b9d135517a33244e65c2411ae4405ceb1ea
yuseefsinan-eng <yuseefsinan@gmail.com>
2026-09-14 01:55:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -18,4 +19,5 @@ def change_language():
     return jsonify({"message": f"زمان هاتە گوهۆڕین بۆ: {selected_lang}"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
